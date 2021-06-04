3e8a3d98e9c05330402be47a74092e62cabd1213
Jonhks <jonhgx@gmail.com>
2021-06-04 14:52:56 -0500

    creando repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [sonando, setSonando] = useState('')
+
+  const notas = [
+    {
+      nombre: 'Do',
+      link: require('./notes/do.wav'),
+      sostenido: true
+    },
+    {
+      nombre: 'Re',
+      link: require('./notes/re.wav'),
+      sostenido: true
+    },
+    {
+      nombre: 'Mi',
+      link: require('./notes/mi.wav')
+    },
+    {
+      nombre: 'Fa',
+      link: require('./notes/fa.wav'),
+      sostenido: true
+    },
+    {
+      nombre: 'Sol',
+      link: require('./notes/sol.wav'),
+      sostenido: true
+    },
+    {
+      nombre: 'La',
+      link: require('./notes/la.wav'),
+      sostenido: true
+    },
+    {
+      nombre: 'Si',
+      link: require('./notes/si.wav')
+    },
+  ]
+
+  const handleClick = (nota) => {
+    setSonando(nota.nombre)
+    const sonido = new Audio(nota.link.default)
+    sonido.play()
+  }
+
+  setTimeout(()=>{
+    setSonando('')
+  },200)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="titulo">
+        <h1>Piano con React</h1>
+        <h2>Hecho por Male 😀</h2>
+        { sonando && <h3>Esta sonando la nota {sonando}</h3>}
+      </div>
+      <div className="container">
+        {notas.map(nota => (
+          <div
+            key={Math.random() * 1000}
+            className={`nota ${sonando === nota.nombre && 'sonando'}`}
+            onClick={() => handleClick(nota)}
+          >
+            {/* {nota.sostenido ? <div className="negra"></div>: null} */}
+            {nota.sostenido && <div className="negra"></div>}
+          </div>
+        ))}
+
+        {/* <div className="nota" onClick={handleClick}>
+          <div className="negra"></div>
+        </div>
+        <div className="nota">
+          <div className="negra"></div>
+        </div>
+        <div className="nota"></div>
+        <div className="nota">
+          <div className="negra"></div>
+        </div>
+        <div className="nota">
+          <div className="negra"></div>
+        </div>
+        <div className="nota">
+          <div className="negra"></div>
+        </div>
+        <div className="nota"></div> */}
+      </div>
     </div>
   );
 }
